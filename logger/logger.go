c02c108f2519057588eeb1f044c3007ca2040721
rain.i <rain.i@kakaoenterprise.com>
2020-05-07 08:19:12 +0900

    logger

---
+++ logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+func GetLogger(logFile string) *log.Logger {
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return log.New(f, "dbmon : ", log.Ldate|log.Ltime)
+}
