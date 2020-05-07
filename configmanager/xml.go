384f46370c7fa9a6bb9afc5093ba6da340817ba6
rain.i <rain.i@kakaoenterprise.com>
2020-05-07 09:34:19 +0900

    xml config manager

---
+++ configmanager/xml.go
@@ -0,0 +1,22 @@
+package configmanager
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+type XmlConfigManager struct {
+	configFile string
+}
+
+func (x *XmlConfigManager) ReadConfig() error {
+	log.Printf("read config from xml config file : %s\n", x.configFile)
+	return nil
+}
+
+func (x XmlConfigManager) ShowConfig() {
+	log.Printf("show configuration : %s", x.configFile)
+}
+
+func CreateXmlConfigManager(configFile string) *XmlConfigManager {
+	return &XmlConfigManager{configFile}
+}
