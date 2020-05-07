5d74db6727f05b632b25b955835b153084b35dad
rain.i <rain.i@kakaoenterprise.com>
2020-05-08 08:40:28 +0900

    config manager

---
+++ config/jsonmanager.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+type JsonManager struct {
+	configFile string
+}
+
+func (j *JsonManager) ReadConfig() error {
+	log.Println("read json config...")
+	return nil
+}
+
+func (j JsonManager) ShowConfig() {
+	log.Println("show json config...")
+}
