0cea6b765d592419c35e27f41fecc6fc1ac5a61c
rain.i <rain.i@kakaoenterprise.com>
2020-05-08 09:04:07 +0900

    config manager

--- configmanager/manager.go
+++ configmanager/manager.go
@@ -1,5 +1,12 @@
 package configmanager
 
+import "errors"
+
+var (
+	InvalidExtension = errors.New("invalid config file extension")
+	InvalidSyntax    = errors.New("invalid config syntax")
+)
+
 type ConfigManager interface {
 	ReadConfig() error
 	ShowConfig()
