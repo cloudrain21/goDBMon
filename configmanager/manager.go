a60eaa518dd8ece33f93395e4eadc7a27b5c2378
rain.i <rain.i@kakaoenterprise.com>
2020-05-08 09:18:00 +0900

    config manager - NewConfigManager

--- configmanager/manager.go
+++ configmanager/manager.go
@@ -7,7 +7,18 @@ var (
 	InvalidSyntax    = errors.New("invalid config syntax")
 )
 
-type ConfigManager interface {
+type InterConfigManager interface {
 	ReadConfig() error
 	ShowConfig()
+}
+
+type ConfigManager struct {
+	mgr InterConfigManager
+}
+
+func NewConfigManager(mgrinter InterConfigManager) *ConfigManager {
+	cmgr := new(ConfigManager)
+
+	cmgr.mgr = mgrinter
+	return cmgr
 }
